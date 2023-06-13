75cd1cf0967cc54d9612063c3f86f991ccad79a2
Patryk Jozwiak <patryk.jozwiak@epassi.com>
2023-06-13 23:20:10 +0200

    Setup of devise authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  enum role: %i[user admin]
+  after_initialize :set_default_role, if: :new_record?
+
+  private
+
+  def set_default_role
+    self.role ||= :user
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-
 Devise.setup do |config|
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
   require 'devise/orm/active_record'
@@ -16,14 +15,15 @@ Devise.setup do |config|
   config.sign_out_via = :delete
   config.responder.error_status = :unprocessable_entity
   config.responder.redirect_status = :see_other
+  config.navigational_formats = []
 
   config.jwt do |jwt|
-    jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
+    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
     jwt.dispatch_requests = [
-      ['POST', %r{users/sign_in}]
+      ['POST', %r{^/login$}]
     ]
     jwt.revocation_requests = [
-      ['DELETE', %r{users/sign_out}]
+      ['DELETE', %r{^/logout$}]
     ]
     jwt.expiration_time = 15.day.to_i
 
