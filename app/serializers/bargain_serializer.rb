7eb6a85048395f0a0b7f789bdd7076a0e988ec53
Patryk Jozwiak <patryk.jozwiak@epassi.com>
2023-07-24 21:30:58 +0200

    BargainSerializer fix

--- app/serializers/bargain_serializer.rb
+++ app/serializers/bargain_serializer.rb
@@ -1,3 +1,3 @@
 class BargainSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :title, :description, :link, :main_image_url
+  attributes :id, :ends_at, :title, :description, :link, :main_image_url
 end
